6def687177944dc1510c480274c51e025ae22338
Trevor Burton <worldofpaper@googlemail.com>
2012-08-21 17:01:53 +0100

    removed the overriding setSource - src/source now alias each other.

--- src/qtv/impl/component/display/image/BasicImage.hx
+++ src/qtv/impl/component/display/image/BasicImage.hx
@@ -10,11 +10,6 @@ class BasicImage extends Image
 {
 	public var source(never, setSource):String;
 	
-	public function setSource(value:String):String {
-		src = value;
-		return value;
-	}
-	
 	public function new() 
 	{
 		super();
