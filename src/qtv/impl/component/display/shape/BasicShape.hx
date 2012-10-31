35fd5421bf5b908b88305650eb964c094dc92912
Trevor Burton <worldofpaper@googlemail.com>
2012-10-31 15:21:23 +0000

    Added the basic shape tool

---
+++ src/qtv/impl/component/display/shape/BasicShape.hx
@@ -0,0 +1,15 @@
+package qtv.impl.component.display.shape;
+
+/**
+ * ...
+ * @author Trevor B
+ */
+
+class BasicShape extends xuloo.ui.BasicShape
+{
+	public function new() 
+	{
+		super();
+	}
+	
+}
