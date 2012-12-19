bed4a4f92c407e92fb9133a7868be03371ba5851
Trevor Burton <worldofpaper@googlemail.com>
2012-12-19 10:20:30 +0000

    fixed the share on social operation

--- src/qtv/impl/component/operation/ShareOnSocialOperation.hx
+++ src/qtv/impl/component/operation/ShareOnSocialOperation.hx
@@ -1,8 +1,10 @@
 package qtv.impl.component.operation;
 
+#if flash
 import flash.external.ExternalInterface;
 import flash.net.URLRequest;
-import flash.net.NavigateToURL;
+#end
+
 import qtv.api.core.Constants;
 
 import xuloo.ui.Action;
@@ -12,7 +14,7 @@ class ShareOnSocialOperation extends Action {
 	public var body(getBody, setBody) : String;
 	public var data(never, setData) : Dynamic;
 
-	static var TWITTER_SHARE_URL : String = "http://twitter.com/home?status=[body]" + escape(" ") + "[url]";
+	static var TWITTER_SHARE_URL : String = "http://twitter.com/home?status=[body]" + StringTools.htmlEscape(" ") + "[url]";
 	static var FACEBOOK_SHARE_URL : String = "http://www.facebook.com/sharer/sharer.php?u=[url]";
 	static var GOOGLE_SHARE_URL : String = "https://apis.google.com/js/plusone.js";
 	var _target : String;
@@ -36,12 +38,15 @@ class ShareOnSocialOperation extends Action {
 	}
 
 	public function setData(value : Dynamic) : Dynamic {
-		if(value.hasOwnProperty("target"))  {
-			_target = value["target"];
+
+		if (Reflect.hasField(value, "body")) {
+			_body = cast Reflect.field(value, "body");
 		}
-		if(value.hasOwnProperty("body"))  {
-			_body = value["body"];
+
+		if (Reflect.hasField(value, "target")) {
+			_target = cast Reflect.field(value, "target");
 		}
+		
 		return value;
 	}
 
@@ -53,7 +58,7 @@ class ShareOnSocialOperation extends Action {
 		var url : String = compileShareURL(_target, _body);
 		// was going to use controlLayer.openPopup but that doesn't do what I expect
 		openURL(url, "Share", _target);
-		controlLayer.setEnabled(true);
+		interactiveLayer.setEnabled(true);
 	}
 
 	public function executeWithData(value : Dynamic) : Void {
@@ -77,21 +82,24 @@ class ShareOnSocialOperation extends Action {
 
 	static function compileShareURL(target : String, body : String) : String {
 		var url : String = getShareURL(target);
-		url = url.replace("[body]", escape(body));
-		url = url.replace("[url]", escape(getPageURL()));
+		url = StringTools.replace(url, "[body]", StringTools.htmlEscape(body));
+		url = StringTools.replace(url, "[url]", StringTools.htmlEscape(getPageURL()));
 		return url;
 	}
 
 	static function openURL(url : String, title : String, target : String) : Void {
+		#if flash
 		if(ExternalInterface.available)  {
 			ExternalInterface.call("window.open", url, title, buildPopupProperties(target));
 		}
-
 		else  {
-			var request : URLRequest = new URLRequest(url);
-			navigateToURL(request);
-		}
 
+			var request : URLRequest = new URLRequest(url);
+			flash.Lib.getURL(request);
+		}
+		#elseif js
+		js.Lib.window.open(url, title, buildPopupProperties(target));
+		#end
 	}
 
 	static function buildPopupProperties(target : String) : String {
@@ -105,14 +113,18 @@ class ShareOnSocialOperation extends Action {
 		case Constants.TWITTER:
 		case Constants.GOOGLE:
 		}
-		properties = properties.replace("[h]", h);
-		properties = properties.replace("[w]", w);
+		properties = StringTools.replace(properties, "[h]", Std.string(h));
+		properties = StringTools.replace(properties, "[w]", Std.string(w));
 		return properties;
 	}
 
 	static function getPageURL() : String {
+		#if flash
 		if(ExternalInterface.available) return ExternalInterface.call("window.location.href.toString")
 		else return "";
+		#elseif js
+		return js.Lib.window.location.href;
+		#end
 	}
 
 }
